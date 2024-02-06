0649b9764bbe7b0bc61d7cf32a56a3ef45a9d421
Anton <krava34007@gmail.com>
2024-02-06 10:28:57 +0000

    add basic Server config

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,9 +1,26 @@
 import express from "express";
 
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import helmet from "helmet";
+
 import { connectDatabase, logger, PORT } from "@/config/index.js";
 import { errorMiddleware, loggerMiddleware } from "@/middlewares/index.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(helmet());
+
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+  }),
+);
 
 app.use(loggerMiddleware);
 app.use(errorMiddleware);
